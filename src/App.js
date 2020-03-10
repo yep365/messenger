900353d2b0d3860b84b13453ec415f1b5750fc2b
yep365 <kacherov01@gmail.com>
2020-03-10 23:00:37 +0300

    Added Auth component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-
-import "./App.css";
+import { Auth } from "./pages";
 
 function App() {
-  return <div className="wrapper">Hello world!</div>;
+  return (
+    <div className="wrapper">
+      <Auth />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Block } from "./components";
+
+const Auth = () => {
+  return (
+    <Block>
+      <Button className="button__large" type="primary" size="large">
+        This is Button
+      </Button>
+    </Block>
+  );
+};
+
+export default Auth;
