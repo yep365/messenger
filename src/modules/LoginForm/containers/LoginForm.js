792b8bc967667b22097f63e299aeb5508eeb8bad
yep365 <kacherov01@gmail.com>
2020-03-14 22:55:48 +0300

    Created validate.js in utils folder. Form validation still has some small issues with red hover effect.

--- src/modules/LoginForm/containers/LoginForm.js
+++ src/modules/LoginForm/containers/LoginForm.js
@@ -1,3 +1,24 @@
+import { withFormik } from "formik";
 import LoginForm from "../components/LoginForm";
+import validateForm from "../../../utils/validate";
 
-export default LoginForm;
+export default withFormik({
+  enableReinitialize: true,
+  mapPropsToValues: () => ({
+    email: "",
+    password: ""
+  }),
+
+  validate: values => {
+    let errors = {};
+    validateForm({ isAuth: true, values, errors });
+    return errors;
+  },
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+  displayName: "LoginForm"
+})(LoginForm);
--- src/modules/RegisterForm/containers/RegisterForm.js
+++ src/modules/RegisterForm/containers/RegisterForm.js
@@ -1,25 +1,11 @@
 import { withFormik } from "formik";
 import RegisterForm from "../components/RegisterForm";
+import validateForm from "../../../utils/validate";
 
 export default withFormik({
   validate: values => {
     let errors = {};
-
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email address";
-    }
-
-    if (!values.password) {
-      errors.password = "Введите пароль";
-    } else if (
-      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/.test(values.password)
-    ) {
-      errors.password = "Слишком лёгкий пароль";
-    }
+    validateForm({ isAuth: false, values, errors });
 
     return errors;
   },
