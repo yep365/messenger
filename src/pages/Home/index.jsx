405b00d4598a9658b097bc1831257f383e3be4cb
yep365 <kacherov01@gmail.com>
2020-03-23 15:11:47 +0300

    Fixed svg.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -97,7 +97,6 @@ const Home = () => {
               avatar="https://sun9-14.userapi.com/c851124/v851124397/1d9009/cD2i0BWhMHo.jpg?ava=1"
               text="Hello World, how are you?"
               date={"Fri Mar 13 2020 14:25:48 GMT+0300"}
-              isMe={true}
               isReaded={true}
               attachments={[
                 {
